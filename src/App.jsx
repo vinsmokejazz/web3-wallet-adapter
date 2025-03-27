3cd701cf09cc5335b69f8ea328f413b4bf18335d
vinsmokejazz <vinsmokejazz@gmail.com>
2025-03-27 22:42:19 +0530

    dnn

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,15 @@ function App() {
 
 
   return (
-    <div className="bg-slate-800">
+    <div className="bg-slate-800 min-h-screen">
       <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/CiNYCjCfB2N8xhLO4IU2drj2kbEHf6_p"}>
-        <WalletProvider wallets={[]}>
+        <WalletProvider wallets={[new UnsafeBurnerWalletAdapter]}>
           <WalletModalProvider>
-            <WalletMultiButton />
-            <WalletDisconnectButton />
+            <div className="fixed top-4 right-4 flex gap-2"> 
+              <WalletMultiButton className="!bg-purple-600 hover:!bg-purple-700 !rounded-xl" />
+            <WalletDisconnectButton  className="!bg-gray-600 hover:!bg-gray-700 !rounded-xl"/> 
+            </div>
+            
 
             <Airdrop />
 
--- src/components/Airdrop.jsx
+++ src/components/Airdrop.jsx
@@ -10,24 +10,48 @@ export const Airdrop=()=>{
   
   async function sendAirdropToUser(){
     try{
+      if(!wallet.publicKey){
+        alert("Please connect to Wallet First")
+        return;
+      }
       const connection = new Connection("https://solana-devnet.g.alchemy.com/v2/CiNYCjCfB2N8xhLO4IU2drj2kbEHf6_p")
+
       const amount = Number(inputRef.current?.value || 0);
+
+      if(amount<=0){
+        alert("Please enter a valid amount");
+        return;
+      }
     
     await connection.requestAirdrop(wallet.publicKey, amount*1000000000)
     alert("airdropped");
+    
   }catch(error){
     alert("Error: " + error.message)
   }
 }
 
-  return <div className="flex flex-col justify-center min-h-screen items-center ">
+  return <div className="flex flex-col justify-center min-h-screen items-center px-4">
 
     <h1 className=" text-7xl transition-colors bg-gradient-to-r from-white to-gray-500 
     bg-clip-text text-transparent font-bold pb-2 ">Gucet</h1>
-    <h2 className="text-xl text-white pb-4"> Free Airdrop SOL to your wallet!!</h2>
+    <h2 className="text-xl text-white mb-4"> Free Airdrop SOL to your wallet!!</h2>
 
-    <input className="w-md h-12 bg-gray-100  rounded-2xl mb-3 items-center p-4" type="text" ref={inputRef} placeholder="Enter SOL"/>
-    <button className="bg-purple-800 w-30 h-10 rounded-2xl text-white items-center" onClick={sendAirdropToUser}>Airdrop SOL</button>
+    <div className="flex flex-col gap-4 w-full max-w-md items-center justify-between">
+    <input className="w-full h-12 bg-gray-700 rounded-xl px-4 text-white 
+                   placeholder-gray-400 focus:outline-none focus:ring-1 
+                   focus:ring-purple-500" type="text" ref={inputRef} 
+                   placeholder="Enter SOL"
+                   step="0.1"
+                   min="0"/>
+
+    <button className="w-35  h-12 bg-purple-600 rounded-xl text-white font-medium
+                   hover:bg-purple-700 transition-colors disabled:opacity-50" 
+                   onClick={sendAirdropToUser}>Airdrop SOL</button>
+
+    </div>
+
+  
   </div>
 
 }
